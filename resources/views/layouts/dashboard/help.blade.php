0fe249d2f0dc30994295fb4fee09b427960bd64d
pa3lo <nardellipv@gmail.com>
2018-02-14 19:58:13 -0300

    add faq

--- resources/views/layouts/dashboard/help.blade.php
+++ resources/views/layouts/dashboard/help.blade.php
@@ -1,80 +1,150 @@
 <div class="card">
-        <div class="header">
-            <h2>
-                Preguntas frecuentes
-                <small>Preguntas frecuentes para poder utilizar correctamente la herremienta. Si tiene otra duda por favor no 
-                    dude en contactarnos al mail nardellipv@gmail.com</small>
-            </h2>        
-        </div>
-        <div class="body">
-            <div class="row clearfix">
-                <div class="col-xs-12 ol-sm-12 col-md-12 col-lg-12">
-                    <div class="panel-group" id="accordion_1" role="tablist" aria-multiselectable="true">
-                            <div class="panel panel-primary">
-                                    <div class="panel-heading" role="tab" id="headingTwo_1">
-                                        <h4 class="panel-title">
-                                            <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion_1" href="#collapseOne_1" aria-expanded="false"
-                                                aria-controls="collapseOne_1">
-                                                        Collapsible Group Item #2
-                                                    </a>
-                                        </h4>
-                                    </div>
-                                    <div id="collapseOne_1" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne_1">
-                                        <div class="panel-body">
-                                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute,
-                                            non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch
-                                            3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
-                                            shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt
-                                            sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer
-                                            farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus
-                                            labore sustainable VHS.
-                                        </div>
-                                    </div>
-                                </div>
+    <div class="header">
+        <h2>
+            Preguntas frecuentes
+            <small>Preguntas frecuentes para poder utilizar correctamente la herremienta. Si tiene otra duda por favor no 
+                dude en contactarnos al mail nardellipv@gmail.com</small>
+        </h2>        
+    </div>
+    <div class="body">
+        <div class="row clearfix">
+            <div class="col-xs-12 ol-sm-12 col-md-12 col-lg-12">
+                <div class="panel-group" id="accordion_1" role="tablist" aria-multiselectable="true">
                         <div class="panel panel-primary">
-                            <div class="panel-heading" role="tab" id="headingTwo_1">
-                                <h4 class="panel-title">
-                                    <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion_1" href="#collapseTwo_1" aria-expanded="false"
-                                        aria-controls="collapseTwo_1">
-                                                Collapsible Group Item #2
-                                            </a>
-                                </h4>
-                            </div>
-                            <div id="collapseTwo_1" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo_1">
-                                <div class="panel-body">
-                                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute,
-                                    non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch
-                                    3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
-                                    shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt
-                                    sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer
-                                    farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus
-                                    labore sustainable VHS.
+                                <div class="panel-heading" role="tab" id="headingTwo_1">
+                                    <h4 class="panel-title">
+                                        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion_1" href="#collapseOne_1" aria-expanded="false"
+                                            aria-controls="collapseOne_1">
+                                                    Inscripción Colegio
+                                                </a>
+                                    </h4>
                                 </div>
+                                <div id="collapseOne_1" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne_1">
+                                    <div class="panel-body">
+                                        Luego de registrarse el sistema no le permitira realizar ninguna operación hasta que complete los datos 
+                                        en la pantalla de perfil del colegio.                                            
+                                    </div>
+                                </div>
+                            </div>
+                    <div class="panel panel-primary">
+                        <div class="panel-heading" role="tab" id="headingTwo_1">
+                            <h4 class="panel-title">
+                                <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion_1" href="#collapseTwo_1" aria-expanded="false"
+                                    aria-controls="collapseTwo_1">
+                                            Inscripción alumnos
+                                        </a>
+                            </h4>
+                        </div>
+                        <div id="collapseTwo_1" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo_1">
+                            <div class="panel-body">
+                                Dentro del menú de <b>Inscripciones</b> usted encontrará el botón <b>inscribir alumnos</b> al ingresar se mostrará
+                                el formulario en el cual deberá registrar todos los alumnos que vayan a participar de las olimpiadas, por favor complete
+                                todos los campos.
                             </div>
                         </div>
-                        <div class="panel panel-primary">
-                            <div class="panel-heading" role="tab" id="headingThree_1">
-                                <h4 class="panel-title">
-                                    <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion_1" href="#collapseThree_1" aria-expanded="false"
-                                        aria-controls="collapseThree_1">
-                                                Collapsible Group Item #3
-                                            </a>
-                                </h4>
+                    </div>
+                    <div class="panel panel-primary">
+                        <div class="panel-heading" role="tab" id="headingTwo_1">
+                            <h4 class="panel-title">
+                                <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion_1" href="#collapseThree_1" aria-expanded="false"
+                                    aria-controls="collapseThree_1">
+                                            Inscripción docentes
+                                        </a>
+                            </h4>
+                        </div>
+                        <div id="collapseThree_1" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo_1">
+                            <div class="panel-body">
+                                Dentro del menú de <b>Inscripciones</b> usted encontrará el botón <b>inscribir docentes</b> al ingresar se mostrará
+                                el formulario en el cual deberá registrar todos los docentes que vayan a participar de las olimpiadas, por favor complete
+                                todos los campos.
                             </div>
-                            <div id="collapseThree_1" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree_1">
-                                <div class="panel-body">
-                                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute,
-                                    non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch
-                                    3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
-                                    shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt
-                                    sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer
-                                    farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus
-                                    labore sustainable VHS.
-                                </div>
+                        </div>
+                    </div>
+                    <div class="panel panel-primary">
+                        <div class="panel-heading" role="tab" id="headingThree_1">
+                            <h4 class="panel-title">
+                                <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion_1" href="#collapseFourth_1" aria-expanded="false"
+                                    aria-controls="collapseFourth_1">
+                                            Listado alumnos
+                                        </a>
+                            </h4>
+                        </div>
+                        <div id="collapseFourth_1" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree_1">
+                            <div class="panel-body">
+                                Dentro del menú <b>listados</b> usted verá el botón <b>listar alumnos</b> al ingresar aquí mostrará el listado de alumnos
+                                que el docente a cargo a registrado para participar en las olimpiadas.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel panel-primary">
+                        <div class="panel-heading" role="tab" id="headingThree_1">
+                            <h4 class="panel-title">
+                                <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion_1" href="#collapseFifth_1" aria-expanded="false"
+                                    aria-controls="collapseFifth_1">
+                                            Listado docentes
+                                        </a>
+                            </h4>
+                        </div>
+                        <div id="collapseFifth_1" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree_1">
+                            <div class="panel-body">
+                                Dentro del menú <b>listados</b> usted verá el botón <b>listar docentes</b> al ingresar aquí mostrará el listado de docentes
+                                que el docente a cargo a registrado para participar en las olimpiadas.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel panel-primary">
+                        <div class="panel-heading" role="tab" id="headingThree_1">
+                            <h4 class="panel-title">
+                                <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion_1" href="#collapseSix_1" aria-expanded="false"
+                                    aria-controls="collapseSix_1">
+                                            Ingreso de notas
+                                        </a>
+                            </h4>
+                        </div>
+                        <div id="collapseSix_1" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree_1">
+                            <div class="panel-body">
+                                En el listado de alumnos, usted verá un botón <b>Agregar Notas</b> desde acá usted podrá ingresar las notas de los alumnos luego de 
+                                haber hecho el exámen. Este botón se encontrará deshabilitado hasta que se hayan tomado los exámenes y estará habilitado solo por un periodo
+                                de tiempo establecido, este periodo se avisará a su tiempo desde el sitio web de la Olimpiada y dentro de esta plataforma.
+                                Los alumnos del nivel 1 solo tendrán una sola nota.
+                                Los alumnos del nivel 2 tendran 2 notas, teoría y practica.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel panel-primary">
+                        <div class="panel-heading" role="tab" id="headingThree_1">
+                            <h4 class="panel-title">
+                                <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion_1" href="#collapseSeven_1" aria-expanded="false"
+                                    aria-controls="collapseSeven_1">
+                                            Filtado de alumnos aprobados
+                                        </a>
+                            </h4>
+                        </div>
+                        <div id="collapseSeven_1" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree_1">
+                            <div class="panel-body">
+                                En el menú <b>Listados</b> se verá un botón <b>Filtar alumnos Aprobados</b> este listado solo mostrará los alumnos que previamente
+                                se ingreso las notas y fueron aprobados.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel panel-primary">
+                        <div class="panel-heading" role="tab" id="headingThree_1">
+                            <h4 class="panel-title">
+                                <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion_1" href="#headingEighth_1" aria-expanded="false"
+                                    aria-controls="headingEighth_1">
+                                        Área de información y descargas
+                                        </a>
+                            </h4>
+                        </div>
+                        <div id="headingEighth_1" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree_1">
+                            <div class="panel-body">
+                                Dentro del <b>Panel Principal</b> se verá un recuadro donde se publicaran las noticias importantes sobre el proceso de las 
+                                olimpiadas. Tabién se veran descargas de archivos importantes.
                             </div>
                         </div>
                     </div>
                 </div>
-    
             </div>
+
         </div>
+    </div>
